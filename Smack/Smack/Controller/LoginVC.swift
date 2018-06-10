618ae3cf5c58071156952e1e2b2385c91e38b33e
Hassan Ashraf <Selv3R@Hassan-Ashrafs-MacBook-Pro.local>
2018-06-10 03:45:20 +0200

    LoginVC Design complete

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Hassan Ashraf on 6/10/18.
+//  Copyright © 2018 h2m. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Hassan Ashraf on 6/9/18.
+//  Copyright © 2018 h2m. All rights reserved.
+//
+
+import Foundation
+
+
+//Segues
+let TO_LOGIN = "toLogin"
