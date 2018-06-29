aefc2514862dfa2c1e97098730a1f467b2b1349a
Hassan Ashraf <Selv3R@Hassan-Ashrafs-MacBook-Pro.local>
2018-06-29 18:42:09 +0200

    ProfileVC and Logout Function

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -23,7 +23,16 @@ class ChannelVC: UIViewController {
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        
+        if AuthService.instance.isLoggedIn == true {
+            //Show Profile Page
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    
+        }
     }
     
     
