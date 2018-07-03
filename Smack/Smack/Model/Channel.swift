0566202ed25613bdcbe7e7f4c5e5d88f0915cbbe
Hassan Ashraf <Selv3R@Hassan-Ashrafs-MacBook-Pro.local>
2018-07-03 20:33:20 +0200

    Get channels from DB

---
+++ Smack/Smack/Model/Channel.swift
@@ -0,0 +1,19 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Hassan Ashraf on 7/3/18.
+//  Copyright © 2018 h2m. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    private(set) public var channelTitle: String!
+    private(set) public var channelDescription: String!
+    private(set) public var id: String!
+    
+    
+    
+}
---
+++ Smack/Smack/Services/MessageService.swift
@@ -0,0 +1,63 @@
+//
+//  MessageService.swift
+//  Smack
+//
+//  Created by Hassan Ashraf on 7/3/18.
+//  Copyright © 2018 h2m. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannels(completion: @escaping CompletionHandler) {
+        
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: AUTH_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else {return}
+                do {
+                    if let json = try JSON(data: data).array {
+                        for item in json {
+                            let name = item["name"].stringValue
+                            let channelDescription = item["description"].stringValue
+                            let id = item["_id"].stringValue
+                            let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                            self.channels.append(channel)
+                        }
+                        print(self.channels)
+                    completion(true)
+                    }
+                
+                } catch let error as NSError{
+                    print(error)
+                }
+            } else {
+                completion(false)
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+} //Class
